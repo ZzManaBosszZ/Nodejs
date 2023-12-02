2af23366e61cdaa339772266fb6bf216221b36a3
Phuong <phuongphuong262204@gmail.com>
2023-12-02 08:41:16 +0700

    rename part 2

--- Class1/01-helloworld.js
+++ Class1/01-helloworld.js
@@ -1,10 +1,20 @@
 const http = require('http')
-const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) =>
-{
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello World!')
+const port = proccess.env.PORT((req, res) => {
+    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+    switch(path){
+        case '':
+            res.writeHead(200, { 'Content-Type': 'text/plain'})
+            res.end('Homepage')
+            break
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/plain'})
+            res.end('About')
+            break
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain'})
+            res.end('Not Found')
+            break
+    }
 })
 
-server.listen(port, () => console.log(`server started on port ${port}; ` + 'press Ctrl-C to terminate....')) 
+Server.listen(port, () => console.log(`server started on port ${port}; ` + 'press Ctrl-C to terminate....'))
