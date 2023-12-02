fdf43a87a94120bc01890ee05b94ce029e16c4ec
Phuong <phuongphuong262204@gmail.com>
2023-12-02 10:56:25 +0700

    new

--- Class1/02-helloworld.js
+++ Class1/02-helloworld.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 3000
 function serveStaticFile(res, path, contentType, responseCode = 200) {
     fs.readFile(__dirname + path, (err, data) => {
         if (err) {
-            res.WriteHead(500, { 'Content-Type': contentType })
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
             return res.end('500 - Internal Error')
         }
-        res.WriteHead(responseCode, { ' Content-Type ': contentType })
+        res.writeHead(responseCode, { ' Content-Type ': contentType })
         res.end(data)
     })
 }
@@ -22,14 +22,11 @@ const server = http.createServer((req, res) => {
         case '/about':
             serveStaticFile(res, '/public/about.html', 'text/html')
             break
-        case '/about':
-            serveStaticFile(res, '/public/about.html', 'text/html')
-            break
         case '/img/logo.png':
             serveStaticFile(res, '/public/img/logo.png', 'image/png')
             break
         default:
-            serveStaticFile(res, '/public/404.html', 'text/html')
+            serveStaticFile(res, '/public/404.html', 'text/html', 404)
             break
     }
 })
