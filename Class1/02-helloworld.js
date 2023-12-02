f2d1c52d92dc459e0c93973ed28a6d73dff6824b
Phuong <phuongphuong262204@gmail.com>
2023-12-02 11:02:07 +0700

    new

--- Class1/02-helloworld.js
+++ Class1/02-helloworld.js
@@ -8,7 +8,7 @@ function serveStaticFile(res, path, contentType, responseCode = 200) {
             res.writeHead(500, { 'Content-Type': 'text/plain' })
             return res.end('500 - Internal Error')
         }
-        res.writeHead(responseCode, { ' Content-Type ': contentType })
+        res.writeHead(responseCode, { 'Content-Type': contentType })
         res.end(data)
     })
 }
