4037dc1a507ad073281edb5f350511759942fa30
Phuong <phuongphuong262204@gmail.com>
2023-12-06 15:50:29 +0700

    new

--- Class3/Chapter1/product.js
+++ Class3/Chapter1/product.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars').engine;
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view-engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
