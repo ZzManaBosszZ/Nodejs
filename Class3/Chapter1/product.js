d4a15ceb725387693236b85b3b7811298a72895e
Phuong <phuongphuong262204@gmail.com>
2023-12-06 15:52:34 +0700

    fix

--- Class3/Chapter1/product.js
+++ Class3/Chapter1/product.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view-engine', 'handlebars');
+app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
