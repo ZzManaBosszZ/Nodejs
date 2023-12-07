c671a3f401d283dfd5b1bad5b8095716cd5ea111
Phuong <phuongphuong262204@gmail.com>
2023-12-07 10:10:10 +0700

    add header footer not yet

--- ASM Nodejs/books.js
+++ ASM Nodejs/books.js
@@ -10,12 +10,19 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 const books = [
-    { name: 'Product 1', image: '/img/product1.jpeg' },
-    { name: 'Product 2', image: '/img/product2.jpeg' },
+    { name: 'The Fault In Our Stars ', image: '/img/fault.webp', message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam iusto laudantium impedit maxime culpa sunt', author: 'Author: John Green' },
+    { name: 'Harry Potter Complete Collection', image: '/img/harry.webp', message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam iusto laudantium impedit maxime culpa sunt', author: 'Author: JK.Rowling' },
+    { name: 'The Hobbit ', image: '/img/hobbit.webp', message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam iusto laudantium impedit maxime culpa sunt', author: 'Author: J.R.R Tolkien' },
+    { name: 'The Lord of the Rings   ', image: '/img/ring.webp', message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam iusto laudantium impedit maxime culpa sunt', author: 'Author: J.R.R Tolkien' },
+    { name: 'Sherlock Holmes ', image: '/img/sherlock.webp', message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam iusto laudantium impedit maxime culpa sunt', author: 'Author: Sir Arthur Conan Doyle' },
 ];
 
 app.get('/', (req, res) => {
     res.render('books', { books });
+});
+
+app.get('/contactus', (req, res) => {
+    res.render('contactus')
 });
 
 const PORT = process.env.PORT || 3000;
