41ba700caada0d5b848aaf80623e45786dec9790
Phuong <phuongphuong262204@gmail.com>
2023-12-09 09:10:20 +0700

    update

---
+++ Class4/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "t2209m_sem3nodejs"
+  };
--- Class4/server.js
+++ Class4/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+    origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -17,13 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to bezkoder application." });
 });
 
-// require("./app/routes/tutorial.routes.js")(app);
+require("./app/routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
