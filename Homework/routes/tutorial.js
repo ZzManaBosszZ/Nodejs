2a0d895750dc70efb6b8a283bc4b783c23b88b52
Phuong <phuongphuong262204@gmail.com>
2023-12-15 21:43:42 +0700

    update

--- Homework/routes/tutorial.js
+++ Homework/routes/tutorial.js
@@ -5,13 +5,11 @@ var axios = require('axios');
 var database = require('../config/database')
 
 router.get('/tutorial', function(req, res) {
-    //list user
     axios({
         url: "http://localhost:8080/api/tutorials",
         method: "GET",
     })
         .then(response => {
-            //res.status(200).json(response.data);
             res.render('tutorials/tutorial', {
                 title: 'Tutorial',
                 data: response.data
